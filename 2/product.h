e5f0e797d979fa1328bd0bac964f71a8f7b56254
Androm3d3 <44888228+Androm3d3@users.noreply.github.com>
2022-01-10 08:29:25 +0100

    Add files via upload
    Question 2

---
+++ 2/product.h
@@ -0,0 +1,19 @@
+#include <string>
+
+class Product {
+	public :
+		Product(std::string title, std::string description, float price, int quantity);
+
+		std::string getTitle() const;
+		std::string getDescription() const;
+		float getPrice() const;
+		int getQuantity() const;
+
+		void updateQuantity(int quantity);
+
+	private :
+		std::string _title;
+		std::string _description;
+		float _price;
+		int _quantity;
+};
